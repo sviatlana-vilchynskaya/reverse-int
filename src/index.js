93dd0696ab9cbad81f315fbf9efe5f41855c14c4
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2022-06-30 17:12:37 +0300

    feat:  complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = Math.abs(n) + "";
+        return Number(n.split("").reverse().join(""));
+    } else {
+        n = n + "";
+        return Number(n.split("").reverse().join(""));
+    }
+};
